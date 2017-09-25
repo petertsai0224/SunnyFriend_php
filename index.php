bbfc304548ab70dbb15f98e3f93c24f051e1b3a7
Peter Tsai <petertsai0224@gmail.com>
2017-09-25 14:55:31 +0800

    0925_ver2

--- index.php
+++ index.php
@@ -83,8 +83,8 @@
         ?>
         </h1>
         -->
-        <h1><?= "hello," . $_GET["name"]; ?>
-        </h1>
+        <h1><?= "hello," . $_GET["name"]; ?></h1>
+
 
         <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
       </div>
